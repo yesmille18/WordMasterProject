73a2bb367ddd968679df37ae6d73a6a48c595440
yesmille18 <112670969+yesmille18@users.noreply.github.com>
2022-09-07 01:22:07 +0900

    Add files via upload

---
+++ WordCRUD.java
@@ -0,0 +1,26 @@
+package com.mycom.word;
+
+public class WordCRUD implements ICRUD{
+	
+	@Override
+	public Object add() {
+		return null;
+	}
+
+	@Override
+	public int update(Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void selectOne(int id) {
+		// TODO Auto-generated method stub	
+	}
+}
---
+++ WordManager.java
@@ -0,0 +1,8 @@
+package com.mycom.word;
+
+public class WordManager {
+	
+	public void start() {
+		System.out.println("WordManager!!!");
+	}
+}
